7c46b8a9f6eb65d89c60f21740a3cdfe8d562a5d
Kevin Chen <kevinmichaelchen@gmail.com>
2022-04-01 13:20:25 -0400

    Initial commit

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/kevinmichaelchen/some-protos-go/pkg/some/protos/greeter/v1beta1"
+	"google.golang.org/grpc"
+)
+
+type Server struct{}
+
+func (s Server) SayHello(ctx context.Context, req *v1beta1.SayHelloRequest) (*v1beta1.SayHelloResponse, error) {
+	return &v1beta1.SayHelloResponse{Message: "foobar"}, nil
+}
+
+func main() {
+	server := grpc.NewServer()
+	srv := Server{}
+	v1beta1.RegisterGreeterServiceServer(server, srv)
+	listener, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", 8080))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	if err := server.Serve(listener); err != nil {
+		log.Fatal(err)
+	}
+}
